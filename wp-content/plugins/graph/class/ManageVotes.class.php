e70dd8b73c5ea6ecf82e18d71dd70384c9c627d3
BuZzi <franckgorin.01@gmail.com>
2013-03-22 14:10:15 +0100

    create ManageVotes.class.php

---
+++ wp-content/plugins/graph/class/ManageVotes.class.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Created by the fat IDE JetBrains PhpStorm.
+ * User: Franck GORIN
+ * Date: 22/03/13 - 11:38
+ */
+
+class ManageVotes {
+
+    /**
+     * Constructeur
+     */
+    public function __construct(){
+
+    }
+
+    public function addVote(){
+
+    }
+
+    public function positiveVote(){
+
+    }
+
+    public function negativeVote(){
+
+    }
+
+    public function createHTMLToVote(){
+
+        $_sHTML = "
+            <form>
+                <input type='' />
+            </form>
+        ";
+
+    }
+
+}
